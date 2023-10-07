487be14deaab22476d00adbf870f00471cb76e89
Sidhanta Nayak <sidhantanayak980@gmail.com>
2023-10-07 21:49:31 +0530

    movie website

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App= ()=>{
         const response= await fetch(`${API_URL}&s=${title}`);
         const data=await response.json();
        
-        setMovies(data.Search);
+        setMovies(data.Search);   
 
     }
     useEffect(()=>{
