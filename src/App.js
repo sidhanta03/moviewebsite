146405467131d2cd7e4476e2113ff0ed38d7b9dd
Sidhanta <sidhantanayak980@gmail.com>
2023-10-07 23:40:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 
 const API_URL= 'http://www.omdbapi.com?apikey=4178ec63';
-
+     
 
 const App= ()=>{
     const [movies, setMovies ]=useState([]);
